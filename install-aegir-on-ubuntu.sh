152da508535ed3a7c6335396c95f026165bb8fb1
Doka <doka@wepoca.net>
2011-01-08 13:00:53 +0100

    add apcid, symlink for drush and passwd

--- install-aegir-on-ubuntu.sh
+++ install-aegir-on-ubuntu.sh
@@ -11,11 +11,11 @@
 # set some parameters: 
 MYHOST="myhost.local"
 MYIP="192.168.2.111"
-AEGIR_PWD='wepoca'
 #
 # software requirements
 #
-apt-get install apache2 php5 php5-cli php5-gd php5-mysql mysql-server postfix rsync git-core unzip
+apt-get install apache2 php5 php5-cli php5-gd php5-mysql mysql-server
+apt-get install postfix rsync git-core unzip acpid
 #
 # DNS Configuration
 # admin.$MYHOST is the Aegir admin interface
@@ -25,7 +25,8 @@ echo $MYIP $MYHOST admin.$MYHOST >> /etc/hosts
 # Aegir user
 adduser --system --group --home /var/aegir aegir
 adduser aegir www-data
-usermod -s /bin/bash -p $AEGIR_PWD aegir
+usermod -s /bin/bash aegir
+passwd aegir
 #
 #
 # PHP Configuration
@@ -48,7 +49,10 @@ wget -O install.sh 'http://git.aegirproject.org/?p=provision.git;a=blob_plain;f=
 su -s /bin/sh aegir -c "sh install.sh"
 #
 #
+# Symlink for Drush:
+ln -s /var/aegir/drush/drush.php /bin/drush
+#
+#
 # Checkpoint / Finished!
 # The installation will provide you with a one-time login URL to stdout or via an e-mail.
 # Use this link to login to your new Aegir site for the first time.
-
