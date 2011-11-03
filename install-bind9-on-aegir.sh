2f5d311e68e7616be6614b34624814d1134b9d76
Doka <doka@wepoca.net>
2011-11-03 14:50:39 +0100

    add apparmor config

--- install-bind9-on-aegir.sh
+++ install-bind9-on-aegir.sh
@@ -4,11 +4,13 @@
 # (install-bind9-on-aegir.sh)
 # on Github: https://github.com/doka/install-aegir-on-ubuntu 
 #
-# run with users with sudo rights
-#
 # this script assumes:
-#    - you have successfully installed Aegir 1.x on Ubuntu server
+#  - you have successfully installed Aegir 1.x on Ubuntu server
+#  - copy this script to the ubuntu server and make it executable
+#       wget https://raw.github.com/doka/install-aegir-on-ubuntu/master/install-bind9-on-aegir.sh
+#       chmod 750 ./install-bind9-on-aegir.sh
 #
+# *******************************************
 #    1. install bind9
 #
 sudo apt-get update
@@ -28,12 +30,28 @@ sudo cp /tmp/aegir /etc/sudoers.d/aegir
 sudo adduser bind aegir
 #
 #
-#    4. include zone files of Aegir into the bind config file
+#    4. include zone files of Aegir into bind config
 echo 'include "/var/aegir/config/bind.conf";' | sudo tee -a /etc/bind/named.conf.local 
 #
 #
-#    5. add nameserver to /etc/resolv.conf file
-echo 'nameserver 127.0.0.1' | sudo tee -a /etc/resolv.conf
+#    5. add nameserver to the beginning of the /etc/resolv.conf file
+sudo cp /etc/resolv.conf /tmp/resolv.conf
+echo 'nameserver 127.0.0.1' | sudo tee /etc/resolv.conf
+cat /tmp/resolv.conf | sudo tee -a /etc/resolv.conf
+#
+#
+#    6. configure apparmor
+# quick&dirty: disable the related apparmor profile
+sudo ln -s /etc/apparmor.d/usr.sbin.named /etc/apparmor.d/disable/
+# sudo apparmor_parser -R /etc/apparmor.d/usr.sbin.named
+sudo /etc/init.d/apparmor reload
+#
+# todo: use apparmor, but put all the Aegir zone files into apparmor scope
+#   like sudo ln -s /var/aegir/config/server_master/bind/zone.d/ /var/lib/bind/aegir
+#
+#
+#    7. restart bind
+sudo /etc/init.d/bind9 restart
 #
 #
 #    Checkpoint
@@ -41,10 +59,10 @@ echo "
 #
 # Checkpoint / But not yet finished!
 #
-#   1. activate the "DNS Support" hosting feature in Aegir frontend 
+#   1. activate the DNS Support hosting feature in Aegir frontend 
 #   2. activate the DNS on the particular server: select the server in Aegir frontend, 
 #      select the DNS service and the bind option. Change the restart command to
 #      sudo rndc reload, and click save.
 #   3. set the nameserver in /etc/resolv.conf file of every other server to point to this
 #      nameserver: put one line nameserver <IP-OF-THIS-NAMESERVER>
-'
+"
